b2756852770a64bb5a470fac9f489c3be0627297
Dennis Sangberg <dennis.sangberg@gmail.com>
2021-04-06 19:28:16 +0200

    dance to be tested

---
+++ 1A_square_dance.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int findDancingNeighbour(int irow, int icol, int nrows, int ncols, int compassRow, int compassCol,vector<vector<int>> isDancing){
+    int tmprow;
+    int tmpcol;
+    int tmpfactor=1;
+    
+    for(int ii=0; ii<nrows*ncols;ii++){
+        tmprow = tmpfactor*compassRow + irow;
+        tmpcol = tmpfactor*compassCol + icol;
+        if (tmprow==nrows||tmpcol==ncols||tmpcol<0||tmprow<0){
+            return tmpfactor;
+        }
+        if (isDancing[tmprow][tmpcol]>0 ){
+            return tmpfactor;
+        }
+        tmpfactor++;
+    }
+    return tmpfactor;
+}
+
+vector<vector<int>> create_dance(int nrows, int ncols, vector<vector<int>> isDancing,int option) {
+    for (int irow=0; irow<nrows; irow++){ 
+        for (int icol=0; icol<ncols; icol++){  
+                   
+            if(option == 1){              
+                isDancing[irow][icol] = 1;
+            }else{
+                if(isDancing[irow][icol] == 2){
+                    isDancing[irow][icol] = 0;
+                }
+            }
+        }
+    }
+    return isDancing;
+}
+
+
+int main(){
+
+
+int ncols;
+int nrows;
+
+
+int tt;
+int interest,tmpinterest;
+int compass[4] = {0,0,-1,1};
+float tmpsum;
+int tmprow, tmpcol,N,tmpfactor;
+bool isEliminated = 0;
+
+cin >> tt;
+for (int it = 0; it < tt; it++){
+    cout << "Case #" << it+1 << ": ";
+    cin >> ncols >> nrows;
+    int danceSkills[nrows][ncols];
+    for (int irow=0; irow<nrows; irow++){ 
+            for (int icol=0; icol<ncols; icol++){
+                cin >> danceSkills[irow][icol];
+            }
+    }
+
+
+
+    vector<int> col(ncols,0);
+    vector<vector<int>> isDancing(nrows,col);
+    isDancing = create_dance(nrows, ncols, isDancing,1);
+    tmpinterest = 0;
+
+    for (int iround = 0; iround < ncols*nrows; iround++){
+        isEliminated = 0;  
+        isDancing = create_dance(nrows, ncols, isDancing,2);  
+        cout << iround << endl;   
+        for (int irow=0; irow<nrows; irow++){ 
+            for (int icol=0; icol<ncols; icol++){
+                if (isDancing[irow][icol]>0){
+                    
+                    tmpinterest = tmpinterest + danceSkills[irow][icol];
+
+                    N = 0;
+                    tmpsum = 0;
+                    for (int ii = 0; ii < 4; ii++){
+                        tmpfactor = findDancingNeighbour(irow, icol, nrows, ncols, compass[ii], compass[3-ii], isDancing);
+                        tmprow = tmpfactor*compass[ii] + irow;
+                        tmpcol = tmpfactor*compass[3-ii] + icol;
+
+                        if (tmprow>=0 && tmpcol>=0 && tmprow<nrows && tmpcol<ncols && isDancing[tmprow][tmpcol] >0){
+                            cout << "irow: " << irow << " icol: " << icol << " sum "<< tmpsum<<  " tmprow: " << tmprow << " tmpcol: " << tmpcol  << " danceing: " << isDancing[tmprow][tmpcol] <<" \n";
+                            tmpsum = tmpsum + danceSkills[tmprow][tmpcol];
+                            N++;                            
+                        }   
+                        
+                        
+                        cout << "irow: " << irow << " icol: " << icol << endl;//"    "<<danceSkills[irow][icol]<<endl;
+                    }
+
+                    if (tmpsum/N>danceSkills[irow][icol]){
+                        isDancing[irow][icol] = 2;
+                        isEliminated = 1;
+                    }
+                }
+            }
+        }
+        //TMP
+        cout << "DANCING\n";
+        for (int irow=0; irow<nrows; irow++){ //CREATE ARRAYS
+            for (int icol=0; icol<ncols; icol++){
+            // CIN
+                cout << isDancing[irow][icol] << " ";
+            }
+            cout << "\n";
+        }//ENDTMP
+        cout << "----------\n";
+        interest = tmpinterest;
+        if(!isEliminated){
+            cout << "TERMINATE \n";
+            break;
+        }
+        
+
+    }
+
+cout << interest << " INTEREST\n";
+
+
+
+
+
+
+
+
+
+}
+}
